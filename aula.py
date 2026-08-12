920ba966491a5c7b8c790665b052f7ce5249c340
NICOLAS DA SILVA LOPES DE OLIVEIRA <u26640@academico.cotuca.unicamp.br>
2026-08-11 22:07:43 -0300

    Adiconei linha 1 desenho"

--- aula.py
+++ aula.py
@@ -1,3 +1,8 @@
 nome=input("digite o seu nome")
-print(f'Voce digitou o nome {nome'
-}
+print(f'Voce digitou o nome {nome}')
+
+print()
+print("Vamos desenhar")
+
+for x in range(10):
+     print("-", end="")
